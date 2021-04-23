191c504fee59753583373e3be7036926b28d690d
Cody Back <codyback1996@gmail.com>
2021-04-23 07:57:07 -0400

    got board

---
+++ Random/Abandon_All_Hope.py
@@ -0,0 +1,11 @@
+red = "\033[91m"
+normal = "\033[0m"
+
+aah1 = " Abandon All Hope !!! "
+raah1 = (red + aah1 + normal)
+
+print (f"{red} Abandon All Hope !!! {normal}")
+print ( raah1 )
+
+while True:
+    print ( raah1 )
