b78c3d3de1b50445f0776b54ac200ce003e27214
Cody Back <codyback1996@gmail.com>
2021-04-23 06:51:40 -0400

    Example of colored text output added

---
+++ Examples/colored_text_output/main.py
@@ -0,0 +1,22 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print("Examples: \n")
+
+print(f"{bcolors.HEADER}HEADER{bcolors.ENDC}")
+print(f"{bcolors.OKBLUE}OKBLUE{bcolors.ENDC}")
+print(f"{bcolors.OKCYAN}OKCYAN{bcolors.ENDC}")
+print(f"{bcolors.WARNING}Warning:{bcolors.ENDC}")
+print(f"{bcolors.FAIL}FAIL{bcolors.ENDC}")
+print(f"{bcolors.ENDC}ENDC:{bcolors.ENDC}")
+print(f"{bcolors.BOLD}BOLD{bcolors.ENDC}")
+print(f"{bcolors.UNDERLINE}UNDERLINE{bcolors.ENDC}")
+
